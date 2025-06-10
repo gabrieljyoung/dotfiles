5b949036a108d84635252915c43aabe6648c277c
Gabriel Young <hi@gjyoung.ca>
2025-06-09 19:07:07 -0500

    Zenbook initial config

--- scripts/.xrandr/auto.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-if xrandr | grep -qF 'HDMI-1 connected'; then
-	source ${__dir}/thinkvision.sh
-else
-	source ${__dir}/laptop.sh
-fi
--- scripts/.xrandr/laptop.sh
+++
@@ -1,2 +0,0 @@
-xrandr --dpi 192 \
-	--output "eDP-1" --primary --scale 1x1 --pos 0x0
